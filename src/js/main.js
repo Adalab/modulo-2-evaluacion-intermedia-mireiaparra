84c0541040133d90726cedeedea3202bcedd352b
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-10-31 09:44:44 +0100

    add functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,33 @@
-const startButton = document.querySelector(".js_button");
+const startButton = document.querySelector(".js-button");
+const choose = document.querySelector(".js-select");
+const text = document.querySelector(".js-text");
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
 
 startButton.addEventListener('click', () => {
-   getRandomNumber(6);
+   compare(getRandomNumber(6));
   });
+
+function choosePlayer() {
+const resultPlayer = parseInt(choose.value);
+return resultPlayer;
+}
+
+function compare(randomNumber) {
+    console.log(choosePlayer());
+    console.log(randomNumber);
+    
+if (choosePlayer() > randomNumber) {
+    console.log("ganas tú");
+}
+
+else if (choosePlayer() === randomNumber) {
+    console.log("empate");
+}
+else {
+    console.log("gana la máquina");
+}
+}
 
