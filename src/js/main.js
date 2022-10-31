9a0f6c1ba0f34c4e5e468e6f69bb9fe1b06cb8b8
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-10-31 10:07:24 +0100

    fix strength function

--- src/js/main.js
+++ src/js/main.js
@@ -3,41 +3,40 @@ const choose = document.querySelector(".js-select");
 const text = document.querySelector(".js-text");
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
-
-function strength (raceNumber) {
-    if (raceNumber === 4) {
-        return 3;
-    } else if (raceNumber === 5) {
-        return 5;
-    }else {
-        return 2;
-    }
+  return Math.ceil(Math.random() * max);
 }
 
-startButton.addEventListener('click', () => {
-   compare(strength(getRandomNumber(5)));
-  });
+function strength(raceNumber) {
+  let strengthResult = "";
+  if (raceNumber === 4) {
+    strengthResult = 3;
+  } else if (raceNumber === 5) {
+    strengthResult = 5;
+  } else {
+    strengthResult = 2;
+  }
+
+  return strengthResult;
+}
+
+startButton.addEventListener("click", () => {
+  compare(strength(getRandomNumber(5)));
+});
 
 function choosePlayer() {
-const resultPlayer = parseInt(choose.value);
-return resultPlayer;
+  const resultPlayer = parseInt(choose.value);
+  return resultPlayer;
 }
 
 function compare(randomNumber) {
-    console.log(choosePlayer());
-    console.log(randomNumber);
-    
-if (choosePlayer() > randomNumber) {
+  console.log(choosePlayer());
+  console.log(randomNumber);
+
+  if (choosePlayer() > randomNumber) {
     text.innerHTML = "¡Ha ganado el Ejército del Bien! Enhorabuena";
+  } else if (choosePlayer() === randomNumber) {
+    text.innerHTML = "Empate";
+  } else {
+    text.innerHTML = "¡Ha ganado el Ejército del Mal! Vuelve a intentarlo";
+  }
 }
-
-else if (choosePlayer() === randomNumber) {
-    text.innerHTML =  "Empate";
-}
-else {
-    text.innerHTML =  "¡Ha ganado el Ejército del Mal! Vuelve a intentarlo";
-}
-}
-
